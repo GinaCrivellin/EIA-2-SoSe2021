c524c0f072acf5d0983abfc890b611fca985533b
Gina <gina.crivellin@gmail.com>
2021-04-30 14:55:46 +0200

    new

--- Steckbrief/02_Memory/script.js
+++ Steckbrief/02_Memory/script.js
@@ -32,9 +32,7 @@ function getSettings() {
         backgroundValue: "pink",
         cardValue: "yellow",
         fontValue: "black",
-        radio1Value: "Times New Roman",
-        radio2Value: "Verdana",
-        radio3Value: "Courier New"
+        radio1Value: "Times New Roman"
     };
     for (let entry of formData) {
         console.log(entry);
@@ -52,10 +50,12 @@ function getSettings() {
         settings.fontValue = fontValue.toString();
         var radio1Value = formData.get("Radio1");
         settings.radio1Value = radio1Value.toString();
-        var radio2Value = formData.get("Radio2");
+        /*
+        var radio2Value: FormDataEntryValue = formData.get("Radio2")!;
         settings.radio2Value = radio2Value.toString();
-        var radio3Value = formData.get("Radio3");
+        var radio3Value: FormDataEntryValue = formData.get("Radio3")!;
         settings.radio3Value = radio3Value.toString();
+        */
     }
     return settings;
 }
@@ -66,6 +66,10 @@ function startGame() {
     var cardNumber = settings.stepperValue;
     var gameStartDate = new Date;
     var min1 = gameStartDate.getMinutes();
+    let bord = document.createElement("div");
+    bord.id = "bord";
+    bord.classList.add("bord");
+    document.body.appendChild(bord);
     var cards = [
         {
             back: "pink",
@@ -105,37 +109,84 @@ function startGame() {
         }
     ];
     var cardRange = [];
-    //var cardsOnField: CardInterface [] = [];
     for (let i = 0; i < cardNumber; i++) {
         cardRange.push(cards[i]);
         cardRange.push(cards[i]);
     }
-    /*
-    for (let i: number = 0; i < cardRange.length; i++){
-        let card1: CardInterface = cardRange[i];
-        let card2: CardInterface = cardRange[i];
-
-        cardsOnField.push(card1);
-        cardsOnField.push(card2);
-        cardRange.splice(cards[1], 1);
-    }
-    */
     shuffle(cardRange);
-    function createCard(_card, _cardSize, _backgroundColor, _cardColor, _fontColor, _fontType) {
-        let bord = document.createElement("div");
-        bord.style.backgroundColor = _backgroundColor;
-        document.body.appendChild(bord);
-        let back = document.createElement("div");
-        back.style.backgroundColor = _cardColor;
-        bord.appendChild(back);
-        let front = document.createElement("div");
-        front.innerHTML = _card.front;
-        front.style.color = _fontColor;
-        front.style.fontFamily = _fontType;
-        bord.appendChild(front);
-    }
     for (let i = 0; i < cardRange.length; i++) {
         createCard(cardRange[i], settings.sliderValue, settings.backgroundValue, settings.cardValue, settings.fontValue, settings.radio1Value);
     }
+}
+let clickedCards = [];
+var allCardsOnField = [];
+function createCard(_card, _cardSize, _backgroundColor, _cardColor, _fontColor, _fontType) {
+    let bord = document.getElementById("bord");
+    bord.style.backgroundColor = _backgroundColor;
+    let front = document.createElement("div");
+    front.classList.add("front");
+    front.innerHTML = _card.front;
+    front.style.color = _fontColor;
+    front.style.fontFamily = _fontType;
+    front.style.width = _cardSize + "px";
+    front.style.height = _cardSize + "px";
+    bord.appendChild(front);
+    let back = document.createElement("div");
+    backColor.classList.add("back");
+    back.style.backgroundColor = _cardColor;
+    back.style.width = _cardSize + "px";
+    back.style.height = _cardSize + "px";
+    bord.appendChild(back);
+    allCardsOnField.push({
+        front: front,
+        back: back
+    });
+    let card = {
+        front: front,
+        back: back
+    };
+    allCardsOnField.push(card);
+    back.addEventListener("click", function () {
+        back.style.display = "none";
+        front.style.display = "block";
+        clickedCards.push(card);
+        if (clickedCards.length == 1) {
+            console.log('one card selected, returning');
+            return;
+        }
+        var card1 = clickedCards[0];
+        var card2 = clickedCards[1];
+        console.log('Two cards are selected');
+        if (clickedCards.length == 2) {
+            console.log('Testing cards...');
+            var areCardsEqual = isEqual(clickedCards[0], clickedCards[1]);
+            console.log(areCardsEqual);
+            if (areCardsEqual == true) {
+                // Nach 2 Sekunden wredne passende Karten ausgeblendet
+                setTimeout(function () {
+                    card1.front.style.display = "none";
+                    card2.front.style.display = "none";
+                    card1.back.style.display = "none";
+                    card2.back.style.display = "none";
+                    allCardsOnField.splice(allCardsOnField.indexOf(card1), 1);
+                    allCardsOnField.splice(allCardsOnField.indexOf(card2), 1);
+                    // Wenn zwei passende Karten gefunden wurden, soll der Spieler wieder klicken können um den nächsten Zug zu machen
+                }, 2000);
+            }
+            if (areCardsEqual == false) {
+                setTimeout(function () {
+                    card1.back.style.display = "block";
+                    card2.back.style.display = "block";
+                    card1.front.style.display = "none";
+                    card2.front.style.display = "none";
+                    // Wenn zwei passende Karten gefunden wurden, soll der Spieler wieder klicken können um den nächsten Zug zu machen
+                }, 2000);
+            }
+            clickedCards = [];
+        }
+    });
+}
+function isEqual(first, second) {
+    return first.front === second.front;
 }
 //# sourceMappingURL=script.js.map
